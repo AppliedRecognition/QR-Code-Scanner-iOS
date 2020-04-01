10195e09e57aef29207322b9daa0c2f592eb580a
Jakub Dolejs <jakubdolejs@gmail.com>
2020-04-01 12:35:46 +0100

    Initial Commit

---
+++ QR Code Scanner/QR_Code_Scanner.h
@@ -0,0 +1,19 @@
+//
+//  QR_Code_Scanner.h
+//  QR Code Scanner
+//
+//  Created by Jakub Dolejs on 01/04/2020.
+//  Copyright © 2020 Applied Recognition. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for QR_Code_Scanner.
+FOUNDATION_EXPORT double QR_Code_ScannerVersionNumber;
+
+//! Project version string for QR_Code_Scanner.
+FOUNDATION_EXPORT const unsigned char QR_Code_ScannerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QR_Code_Scanner/PublicHeader.h>
+
+
